38183d70ee9aa7d21edc558d066d93a81926591d
Hammadtahir1 <156454748+Hammadtahir1@users.noreply.github.com>
2024-01-19 15:09:58 +0500

    Add files via upload
    commit

---
+++ Weather App/Weather App/Weather App/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Weather App
+//
+//  Created by Hamza on 19/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+            
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather App/Weather App/Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Hamza on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
